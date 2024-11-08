c0c6931f300d110a4ba66ffd95d39f7d0125ca3b
Glenn Jahnke <gleenn@apple.com>
2024-11-07 23:44:00 -0800

    beads

--- src/hello_quil/core.clj
+++ src/hello_quil/core.clj
@@ -15,7 +15,18 @@
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
   {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+   :angle (+ (:angle state) 0.05)})
+
+
+(defn d [angle b]
+  (let [x (* 300 (q/cos angle))
+        y (* 100 (q/sin angle))]
+    (q/with-translation [(/ (q/width) 2)
+                         (/ (q/height) 2)]
+                        (if b
+                          (q/ellipse x y 100 100)
+                          (q/ellipse y x 100 100)))))
+
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
@@ -23,26 +34,20 @@
   ; Set circle color.
   (q/fill (:color state) 255 255)
   ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  (doseq [i (range 30)]
+    (d (+ (:angle state) i) (zero? (mod i 2)))))
 
 
 (q/defsketch hello-quil
-  :title "You spin my circle right round"
-  :size [500 500]
-  ; setup function called only once, during sketch initialization.
-  :setup setup
-  ; update-state is called on each iteration before draw-state.
-  :update update-state
-  :draw draw-state
-  :features [:keep-on-top]
-  ; This sketch uses functional-mode middleware.
-  ; Check quil wiki for more info about middlewares and particularly
-  ; fun-mode.
-  :middleware [m/fun-mode])
+             :title "You spin my circle right round"
+             :size [800 800]
+             ; setup function called only once, during sketch initialization.
+             :setup setup
+             ; update-state is called on each iteration before draw-state.
+             :update update-state
+             :draw draw-state
+             :features [:keep-on-top]
+             ; This sketch uses functional-mode middleware.
+             ; Check quil wiki for more info about middlewares and particularly
+             ; fun-mode.
+             :middleware [m/fun-mode])
